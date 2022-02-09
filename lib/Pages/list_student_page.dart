9d341b3cf0332fd7ad02cf33316bdd4b4c5bbe4c
Subhanshu20101 <rocksubhanshu@gmail.com>
2022-02-10 03:35:28 +0530

    i=1

--- lib/Pages/list_student_page.dart
+++ lib/Pages/list_student_page.dart
@@ -188,7 +188,7 @@ class _ListStudentPageState extends State<ListStudentPage> {
                       ),
                     ],
                   ),
-                  for (var i = 0; i < storedocs.length; i++) ...[
+                  for (var i = 1; i < storedocs.length; i++) ...[
                     TableRow(
                       children: [
                         TableCell(
